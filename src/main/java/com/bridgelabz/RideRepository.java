4a5b1bdef1266258afd1f9c17b99199f939acab4
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-09-26 17:10:58 +0530

    Cab Invoice Generator

---
+++ src/main/java/com/bridgelabz/RideRepository.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RideRepository {
+    static List<Customer> customerList = new ArrayList<>();
+
+    static Invoice generateInvoiceByUserId(int userId) {
+        for (Customer customer : customerList) {
+            if (customer.getUserId() == userId) {
+                return CabInvoice.generateInvoice(customer.getRides());
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -0,0 +1,93 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CabInvoiceTest {
+
+
+    @Test
+    void givenDistanceAndTimeShouldReturnFare(){
+        CabInvoice cabInvoice = new CabInvoice();
+        double totalFare = cabInvoice.calculateFare(10,5);
+        Assertions.assertEquals(105.0,totalFare);
+    }
+
+    @Test
+    void givenDistanceAndTimeShouldReturnMinimumFare(){
+        CabInvoice cabInvoice = new CabInvoice();
+        double totalFare = cabInvoice.calculateFare(0.2,1);
+        Assertions.assertEquals(5.0,totalFare);
+    }
+
+    @Test
+    void givenMultipleRidesShouldReturnAggregateTotalFare(){
+        Ride ride1 = new Ride(Ride.Category.NORMAL_RIDE,10,5);
+        Ride ride2 = new Ride(Ride.Category.NORMAL_RIDE,20,10);
+        Ride ride3 = new Ride(Ride.Category.NORMAL_RIDE,0.2,1);
+
+        Ride[] rides = {ride1,ride2,ride3};
+        double totalFare = CabInvoice.calculateFare(rides);
+        Assertions.assertEquals(320.0,totalFare);
+    }
+
+    @Test
+    void givenMultipleRideShouldReturnInvoice(){
+        Ride ride1 = new Ride(Ride.Category.NORMAL_RIDE,10,5);
+        Ride ride2 = new Ride(Ride.Category.NORMAL_RIDE,20,10);
+        Ride ride3 = new Ride(Ride.Category.NORMAL_RIDE,0.2,1);
+
+        Ride[] rides = {ride1,ride2,ride3};
+        Invoice actualInvoice = CabInvoice.generateInvoice(rides);
+
+        Invoice expectedInvoice = new Invoice(3,320.0,(320.0/3));
+        Assertions.assertEquals(expectedInvoice,actualInvoice);
+    }
+
+    @Test
+    void givenUserIdShouldReturnTheInvoice(){
+        Ride ride1 = new Ride(Ride.Category.NORMAL_RIDE,10,5);
+        Ride ride2 = new Ride(Ride.Category.NORMAL_RIDE,20,10);
+        Ride ride3 = new Ride(Ride.Category.NORMAL_RIDE,0.2,1);
+        Ride[] rides1 = {ride1,ride2,ride3};
+
+        Ride ride4 = new Ride(Ride.Category.PREMIUM_RIDE,24,12);
+        Ride ride5 = new Ride(Ride.Category.PREMIUM_RIDE,15,5);
+        Ride ride6 = new Ride(Ride.Category.PREMIUM_RIDE,0.2,1);
+        Ride[] rides2 = {ride4,ride5,ride6};
+
+        Customer customer1 = new Customer(1,rides1);
+        Customer customer2 = new Customer(2,rides2);
+
+
+        RideRepository.customerList.add(customer1);
+        RideRepository.customerList.add(customer2);
+
+        Invoice actualInvoice = RideRepository.generateInvoiceByUserId(2);
+        Invoice expectedInvoice = new Invoice(3,639.0,(639.0/3));
+        Assertions.assertEquals(expectedInvoice,actualInvoice);
+    }
+
+    @Test
+    void givenUserIdShouldReturnTheInvoiceForRespectiveRides(){
+        Ride ride1 = new Ride(Ride.Category.NORMAL_RIDE,10,5);
+        Ride ride2 = new Ride(Ride.Category.NORMAL_RIDE,20,10);
+        Ride ride3 = new Ride(Ride.Category.NORMAL_RIDE,0.2,1);
+        Ride[] rides1 = {ride1,ride2,ride3};
+
+        Ride ride4 = new Ride(Ride.Category.PREMIUM_RIDE,24,12);
+        Ride ride5 = new Ride(Ride.Category.PREMIUM_RIDE,15,5);
+        Ride ride6 = new Ride(Ride.Category.PREMIUM_RIDE,0.2,1);
+        Ride[] rides2 = {ride4,ride5,ride6};
+
+        Customer customer1 = new Customer(1,rides1);
+        Customer customer2 = new Customer(2,rides2);
+
+        RideRepository.customerList.add(customer1);
+        RideRepository.customerList.add(customer2);
+
+        Invoice actualInvoice= RideRepository.generateInvoiceByUserId(1);
+        Invoice expectedInvoice = new Invoice(3,320.0,(320.0/3));
+        Assertions.assertEquals(expectedInvoice,actualInvoice);
+    }
+}
